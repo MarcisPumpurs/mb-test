5ae05cc4948e2a0d59a17541e42ede09b446d914
Marcis Pumpurs <marcis.pumpurs@gmail.com>
2021-02-28 09:30:27 +0200

    Basic structure and first page sketch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BottomComponent } from './bottom/bottom.component';
+import { MainComponent } from './main/main.component';
+import { SubscribeComponent } from './subscribe/subscribe.component';
+import { TopComponent } from './top/top.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: SubscribeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopComponent } from './top/top.component';
+import { BottomComponent } from './bottom/bottom.component';
+import { SubscribeComponent } from './subscribe/subscribe.component';
+import { MainComponent } from './main/main.component';
+import { SideComponent } from './side/side.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopComponent,
+    BottomComponent,
+    SubscribeComponent,
+    MainComponent,
+    SideComponent
   ],
   imports: [
     BrowserModule,
